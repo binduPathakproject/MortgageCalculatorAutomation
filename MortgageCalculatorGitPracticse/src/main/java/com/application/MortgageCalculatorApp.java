44f5e4b8749e7362e278e3cdf499733a7675b7ec
bindu <bindu@BinduDell>
2024-01-28 11:44:57 -0500

    adding main method and print statement

--- MortgageCalculatorGitPracticse/src/main/java/com/application/MortgageCalculatorApp.java
+++ MortgageCalculatorGitPracticse/src/main/java/com/application/MortgageCalculatorApp.java
@@ -5,5 +5,8 @@ package com.application;
  * @author Bindu
  */
 public class MortgageCalculatorApp {
+	public static void main(String[] args) {
+		System.out.println("Mortgage Calculator application is starting ....");
+	}
 
 }
